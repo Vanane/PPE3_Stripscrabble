7db0ead1486d5fab0053fbd45777fb62f27f20c9
Vanane <blackfox248@gmail.com>
2018-10-09 16:57:28 +0200

    Ajout de FormVueMenu, à retravailler

---
+++ PPE3_Stripscrabble/FormVueMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE3_Stripscrabble
+{
+    public partial class FormVueMenu : Form
+    {
+        public FormVueMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FormVueMenu_Load(object sender, EventArgs e)
+        {
+            lblTest.Text = string.Format("Bienvenue sur l'application GSB, {0} {1}", Modele.getPrenom(), Modele.getNom());
+        }
+    }
+}
