6687be70b5aa958e21494385a265a1e544758310
Vanane <blackfox248@gmail.com>
2018-10-09 16:56:42 +0200

    Suppression de Form1 Ajout de FormVueConnexion et du program.cs

---
+++ PPE3_Stripscrabble/FormVueConnexionUtilisateur.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PPE3_Stripscrabble
+{
+    partial class FormVueConnexionUtilisateur
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMDP = new System.Windows.Forms.Label();
+            this.labelIdentifiant = new System.Windows.Forms.Label();
+            this.textBoxMDP = new System.Windows.Forms.TextBox();
+            this.textBoxIdentifiant = new System.Windows.Forms.TextBox();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.buttonConnexion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelMDP
+            // 
+            this.labelMDP.AutoSize = true;
+            this.labelMDP.Font = new System.Drawing.Font("Calibri", 10F);
+            this.labelMDP.Location = new System.Drawing.Point(86, 230);
+            this.labelMDP.Name = "labelMDP";
+            this.labelMDP.Size = new System.Drawing.Size(90, 17);
+            this.labelMDP.TabIndex = 0;
+            this.labelMDP.Text = "Mot de passe :";
+            // 
+            // labelIdentifiant
+            // 
+            this.labelIdentifiant.AutoSize = true;
+            this.labelIdentifiant.Font = new System.Drawing.Font("Calibri", 10F);
+            this.labelIdentifiant.Location = new System.Drawing.Point(102, 192);
+            this.labelIdentifiant.Name = "labelIdentifiant";
+            this.labelIdentifiant.Size = new System.Drawing.Size(74, 17);
+            this.labelIdentifiant.TabIndex = 1;
+            this.labelIdentifiant.Text = "Identifiant :";
+            // 
+            // textBoxMDP
+            // 
+            this.textBoxMDP.Location = new System.Drawing.Point(182, 228);
+            this.textBoxMDP.Name = "textBoxMDP";
+            this.textBoxMDP.PasswordChar = '●';
+            this.textBoxMDP.Size = new System.Drawing.Size(200, 20);
+            this.textBoxMDP.TabIndex = 2;
+            // 
+            // textBoxIdentifiant
+            // 
+            this.textBoxIdentifiant.Location = new System.Drawing.Point(182, 190);
+            this.textBoxIdentifiant.Name = "textBoxIdentifiant";
+            this.textBoxIdentifiant.Size = new System.Drawing.Size(200, 20);
+            this.textBoxIdentifiant.TabIndex = 3;
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::PPE3_Stripscrabble.Properties.Resources.logo;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(30, 15);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(424, 169);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 4;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // buttonConnexion
+            // 
+            this.buttonConnexion.Font = new System.Drawing.Font("Calibri", 10F);
+            this.buttonConnexion.Location = new System.Drawing.Point(200, 264);
+            this.buttonConnexion.Name = "buttonConnexion";
+            this.buttonConnexion.Size = new System.Drawing.Size(100, 30);
+            this.buttonConnexion.TabIndex = 5;
+            this.buttonConnexion.Text = "Connexion";
+            this.buttonConnexion.UseVisualStyleBackColor = true;
+            this.buttonConnexion.Click += new System.EventHandler(this.buttonConnexion_Click);
+            // 
+            // FormVueConnexionUtilisateur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 312);
+            this.Controls.Add(this.buttonConnexion);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.textBoxIdentifiant);
+            this.Controls.Add(this.textBoxMDP);
+            this.Controls.Add(this.labelIdentifiant);
+            this.Controls.Add(this.labelMDP);
+            this.Name = "FormVueConnexionUtilisateur";
+            this.Padding = new System.Windows.Forms.Padding(27);
+            this.Text = "Se connecter à l\'application";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMDP;
+        private System.Windows.Forms.Label labelIdentifiant;
+        private System.Windows.Forms.TextBox textBoxMDP;
+        private System.Windows.Forms.TextBox textBoxIdentifiant;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Button buttonConnexion;
+    }
+}
+
---
+++ PPE3_Stripscrabble/FormVueConnexionUtilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE3_Stripscrabble
+{
+    public partial class FormVueConnexionUtilisateur : Form
+    {
+        public FormVueConnexionUtilisateur()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnexion_Click(object sender, EventArgs e)
+        {
+            if (textBoxIdentifiant.Text != "" && textBoxMDP.Text != "" &&
+                Modele.verifierConnexion(textBoxIdentifiant.Text, textBoxMDP.Text)) //Si les données sont valides
+            {
+                Console.WriteLine("Utilisateur Connecté");
+                MessageBox.Show(("Vous êtes connnecté ! Bienvenue, " + Modele.getIdentifiant() + " !"), "Connexion Établie", MessageBoxButtons.OK);
+                FormVueMenu FVM = new FormVueMenu();
+                FVM.Show();
+
+            }
+            else
+            {
+                //Sinon les valeurs sont nulles.
+                MessageBox.Show("L'identifiant ou le mot de passe est incorrect.", "   Erreur de saisie   ", MessageBoxButtons.OK);
+            }
+             
+        }
+    }
+}
