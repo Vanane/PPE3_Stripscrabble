8f9b9696f9dffc2a583e709453bb7570f39eaaee
Vanane <blackfox248@gmail.com>
2018-11-13 16:25:21 +0100

    Ajout du travail de Vanane. Mot de passe modifiable.

---
+++ PPE3_Stripscrabble/FormVueGestionUtilisateur.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PPE3_Stripscrabble
+{
+    partial class FormVueGestionUtilisateur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUserNom = new System.Windows.Forms.TextBox();
+            this.textBoxUserPrenom = new System.Windows.Forms.TextBox();
+            this.textBoxUserRue = new System.Windows.Forms.TextBox();
+            this.textBoxUserId = new System.Windows.Forms.TextBox();
+            this.textBoxUserVille = new System.Windows.Forms.TextBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblPrenom = new System.Windows.Forms.Label();
+            this.lblRue = new System.Windows.Forms.Label();
+            this.lblVille = new System.Windows.Forms.Label();
+            this.textBoxUserMdp = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblMdp = new System.Windows.Forms.Label();
+            this.buttonModifierInfo = new System.Windows.Forms.Button();
+            this.buttonModifierMdp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUserNom
+            // 
+            this.textBoxUserNom.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserNom.Location = new System.Drawing.Point(210, 30);
+            this.textBoxUserNom.Name = "textBoxUserNom";
+            this.textBoxUserNom.ReadOnly = true;
+            this.textBoxUserNom.Size = new System.Drawing.Size(150, 31);
+            this.textBoxUserNom.TabIndex = 0;
+            // 
+            // textBoxUserPrenom
+            // 
+            this.textBoxUserPrenom.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserPrenom.Location = new System.Drawing.Point(30, 30);
+            this.textBoxUserPrenom.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.textBoxUserPrenom.Name = "textBoxUserPrenom";
+            this.textBoxUserPrenom.ReadOnly = true;
+            this.textBoxUserPrenom.Size = new System.Drawing.Size(150, 31);
+            this.textBoxUserPrenom.TabIndex = 1;
+            // 
+            // textBoxUserRue
+            // 
+            this.textBoxUserRue.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserRue.Location = new System.Drawing.Point(30, 94);
+            this.textBoxUserRue.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.textBoxUserRue.Name = "textBoxUserRue";
+            this.textBoxUserRue.ReadOnly = true;
+            this.textBoxUserRue.Size = new System.Drawing.Size(330, 31);
+            this.textBoxUserRue.TabIndex = 2;
+            // 
+            // textBoxUserId
+            // 
+            this.textBoxUserId.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserId.Location = new System.Drawing.Point(30, 282);
+            this.textBoxUserId.Margin = new System.Windows.Forms.Padding(47, 3, 3, 3);
+            this.textBoxUserId.Name = "textBoxUserId";
+            this.textBoxUserId.ReadOnly = true;
+            this.textBoxUserId.Size = new System.Drawing.Size(100, 31);
+            this.textBoxUserId.TabIndex = 3;
+            // 
+            // textBoxUserVille
+            // 
+            this.textBoxUserVille.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserVille.Location = new System.Drawing.Point(30, 158);
+            this.textBoxUserVille.Margin = new System.Windows.Forms.Padding(3, 3, 3, 90);
+            this.textBoxUserVille.Name = "textBoxUserVille";
+            this.textBoxUserVille.ReadOnly = true;
+            this.textBoxUserVille.Size = new System.Drawing.Size(330, 31);
+            this.textBoxUserVille.TabIndex = 4;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(30, 14);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(35, 13);
+            this.lblNom.TabIndex = 5;
+            this.lblNom.Text = "Nom :";
+            // 
+            // lblPrenom
+            // 
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(207, 14);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(49, 13);
+            this.lblPrenom.TabIndex = 6;
+            this.lblPrenom.Text = "Prénom :";
+            // 
+            // lblRue
+            // 
+            this.lblRue.AutoSize = true;
+            this.lblRue.Location = new System.Drawing.Point(27, 78);
+            this.lblRue.Name = "lblRue";
+            this.lblRue.Size = new System.Drawing.Size(51, 13);
+            this.lblRue.TabIndex = 7;
+            this.lblRue.Text = "Adresse :";
+            // 
+            // lblVille
+            // 
+            this.lblVille.AutoSize = true;
+            this.lblVille.Location = new System.Drawing.Point(27, 142);
+            this.lblVille.Name = "lblVille";
+            this.lblVille.Size = new System.Drawing.Size(32, 13);
+            this.lblVille.TabIndex = 8;
+            this.lblVille.Text = "Ville :";
+            // 
+            // textBoxUserMdp
+            // 
+            this.textBoxUserMdp.Font = new System.Drawing.Font("Calibri Light", 14.25F);
+            this.textBoxUserMdp.Location = new System.Drawing.Point(260, 282);
+            this.textBoxUserMdp.Margin = new System.Windows.Forms.Padding(47, 3, 3, 3);
+            this.textBoxUserMdp.Name = "textBoxUserMdp";
+            this.textBoxUserMdp.ReadOnly = true;
+            this.textBoxUserMdp.Size = new System.Drawing.Size(100, 31);
+            this.textBoxUserMdp.TabIndex = 9;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(30, 266);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(59, 13);
+            this.lblLogin.TabIndex = 10;
+            this.lblLogin.Text = "Identifiant :";
+            // 
+            // lblMdp
+            // 
+            this.lblMdp.AutoSize = true;
+            this.lblMdp.Location = new System.Drawing.Point(257, 266);
+            this.lblMdp.Name = "lblMdp";
+            this.lblMdp.Size = new System.Drawing.Size(77, 13);
+            this.lblMdp.TabIndex = 11;
+            this.lblMdp.Text = "Mot de passe :";
+            // 
+            // buttonModifierInfo
+            // 
+            this.buttonModifierInfo.Location = new System.Drawing.Point(285, 195);
+            this.buttonModifierInfo.Name = "buttonModifierInfo";
+            this.buttonModifierInfo.Size = new System.Drawing.Size(75, 23);
+            this.buttonModifierInfo.TabIndex = 12;
+            this.buttonModifierInfo.Text = "Modifier";
+            this.buttonModifierInfo.UseVisualStyleBackColor = true;
+            this.buttonModifierInfo.Click += new System.EventHandler(this.buttonModifierInfo_Click);
+            // 
+            // buttonModifierMdp
+            // 
+            this.buttonModifierMdp.Location = new System.Drawing.Point(285, 319);
+            this.buttonModifierMdp.Name = "buttonModifierMdp";
+            this.buttonModifierMdp.Size = new System.Drawing.Size(75, 23);
+            this.buttonModifierMdp.TabIndex = 13;
+            this.buttonModifierMdp.Text = "Modifier";
+            this.buttonModifierMdp.UseVisualStyleBackColor = true;
+            this.buttonModifierMdp.Click += new System.EventHandler(this.buttonModifierMdp_Click);
+            // 
+            // FormVueGestionUtilisateur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 402);
+            this.Controls.Add(this.buttonModifierMdp);
+            this.Controls.Add(this.buttonModifierInfo);
+            this.Controls.Add(this.lblMdp);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.textBoxUserMdp);
+            this.Controls.Add(this.lblVille);
+            this.Controls.Add(this.lblRue);
+            this.Controls.Add(this.lblPrenom);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.textBoxUserVille);
+            this.Controls.Add(this.textBoxUserId);
+            this.Controls.Add(this.textBoxUserRue);
+            this.Controls.Add(this.textBoxUserPrenom);
+            this.Controls.Add(this.textBoxUserNom);
+            this.MaximizeBox = false;
+            this.Name = "FormVueGestionUtilisateur";
+            this.Padding = new System.Windows.Forms.Padding(27, 27, 6, 6);
+            this.Text = "Informations de l\'utilisateur";
+            this.Load += new System.EventHandler(this.FormVueGestionUtilisateur_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserNom;
+        private System.Windows.Forms.TextBox textBoxUserPrenom;
+        private System.Windows.Forms.TextBox textBoxUserRue;
+        private System.Windows.Forms.TextBox textBoxUserId;
+        private System.Windows.Forms.TextBox textBoxUserVille;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblPrenom;
+        private System.Windows.Forms.Label lblRue;
+        private System.Windows.Forms.Label lblVille;
+        private System.Windows.Forms.TextBox textBoxUserMdp;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblMdp;
+        private System.Windows.Forms.Button buttonModifierInfo;
+        private System.Windows.Forms.Button buttonModifierMdp;
+    }
+}
